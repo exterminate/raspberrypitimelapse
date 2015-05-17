9c789cf2922752c11685b4185dce37de6e58d29a
Ian <ian.coates@gmail.com>
2015-05-17 16:34:53 +0100

    update

--- app2.py
+++ app2.py
@@ -3,9 +3,9 @@ import time
 import emails
 from usernamedetails import Und
 details = Und()
-FRAMES = 10
+FRAMES = 24
 FPS_IN = 10
-FPS_OUT = 24
+FPS_OUT = 12
 TIMEBETWEEN = 6
 FILMLENGTH = float(FRAMES / FPS_IN)
 
@@ -17,12 +17,12 @@ while frameCount < FRAMES:
     frameCount += 1
     time.sleep(TIMEBETWEEN - 6) #Takes roughly 6 seconds to take a picture
 
-os.system("avconv -r %s -i image%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=2592:1458,scale=1280:720 timelapse.gif"%(FPS_IN,'%7d',FPS_OUT))
+os.system("avconv -r %s -i image%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=2592:1458,scale=1280:720 timelapse.mp4"%(FPS_IN,'%7d',FPS_OUT))
 
 message = emails.html(html="<p>Hi!<br>Here is your image...",
                        subject="Image",
                        mail_from=('Ian', details.email))
 #message.attach(data=open(thefilename), filename="timelapse.mp4")
-message.attach(filename="timelapse.gif")
+message.attach(filename="timelapse.mp4")
 r = message.send(to=details.email, smtp={"host": "smtp.gmail.com", "port": 465, "ssl": True, "user": details.user, "password": details.password, "timeout": 5})
 assert r.status_code == 250
