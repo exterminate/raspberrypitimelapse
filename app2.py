43f1fef3484d520959471f3eaab46f8e55766d2b
Ian <ian.coates@gmail.com>
2015-05-17 12:32:24 +0100

    update

--- app2.py
+++ app2.py
@@ -19,10 +19,10 @@ while frameCount < FRAMES:
 
 os.system("avconv -r %s -i image%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=2592:1458,scale=1280:720 timelapse.mp4"%(FPS_IN,'%7d',FPS_OUT))
 
-thefilename = ""
 message = emails.html(html="<p>Hi!<br>Here is your image...",
                        subject="Image",
                        mail_from=('Ian', details.email))
-message.attach(data=open(thefilename), filename="timelapse.mp4")
+#message.attach(data=open(thefilename), filename="timelapse.mp4")
+message.attach(filename="timelapse.mp4")
 r = message.send(to=details.email, smtp={"host": "smtp.gmail.com", "port": 465, "ssl": True, "user": details.user, "password": details.password, "timeout": 5})
 assert r.status_code == 250
