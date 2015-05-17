d0e14dd15a605f28730d831d5c44c3b71598fd36
Ian <ian.coates@gmail.com>
2015-05-17 11:08:03 +0100

    added giffing

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ with picamera.PiCamera() as camera:
             break   
         sleep(30) 
 
-os.system("avconv -r %s -i img%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=2592:1458,scale=1280:720 timelapse.mp4"%(FPS_IN,'%3d',FPS_OUT))
+os.system("avconv -r %s -i img%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=1296:729,scale=1280:720 timelapse.mp4"%(FPS_IN,'%3d',FPS_OUT))
 
 message.attach(data=open(thefilename), filename="timelapse.mp4")
 r = message.send(to=details.email, smtp={"host": "smtp.gmail.com", "port": 465, "ssl": True, "user": details.user, "password": details.password, "timeout": 5})
