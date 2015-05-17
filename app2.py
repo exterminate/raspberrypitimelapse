8f34935b67e1879c8453a52502f1de69080ff798
Ian <ian.coates@gmail.com>
2015-05-17 16:13:35 +0100

    will it save as a gif?

--- app2.py
+++ app2.py
@@ -17,12 +17,12 @@ while frameCount < FRAMES:
     frameCount += 1
     time.sleep(TIMEBETWEEN - 6) #Takes roughly 6 seconds to take a picture
 
-os.system("avconv -r %s -i image%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=2592:1458,scale=1280:720 timelapse.mp4"%(FPS_IN,'%7d',FPS_OUT))
+os.system("avconv -r %s -i image%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=2592:1458,scale=1280:720 timelapse.gif"%(FPS_IN,'%7d',FPS_OUT))
 
 message = emails.html(html="<p>Hi!<br>Here is your image...",
                        subject="Image",
                        mail_from=('Ian', details.email))
 #message.attach(data=open(thefilename), filename="timelapse.mp4")
-message.attach(filename="timelapse.mp4")
+message.attach(filename="timelapse.gif")
 r = message.send(to=details.email, smtp={"host": "smtp.gmail.com", "port": 465, "ssl": True, "user": details.user, "password": details.password, "timeout": 5})
 assert r.status_code == 250
