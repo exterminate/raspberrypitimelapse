aa4397c74e7e5c0130478063c7bb192cdc69f50d
Ian <ian.coates@gmail.com>
2015-05-18 19:54:41 +0100

    update

--- app2.py
+++ app2.py
@@ -23,11 +23,11 @@ os.system("avconv -r %s -i image%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf c
 message = emails.html(html="<p>Hi!<br>Here is your image...",
                        subject="Image",
                        mail_from=('Ian', details.email))
-#thefilename = "timelapse.mp4"
+thefilename = "timelapse.mp4"
 #message.attach(data=open(thefilename), filename="timelapse.mp4")
 
-os.system("avconv -i timelapse.mp4 -s qvga -vf format=rgb8,format=rgb24 -pix_fmt rgb24 -r 10 output.gif")
+os.system("avconv -r %s -i img%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=1296:729,scale=1280:720 timelapse.mp4"%(FPS_IN,'%3d',FPS_OUT))
 
-message.attach(filename="output.gif")
+message.attach(data=open(thefilename), filename="timelapse.mp4")
 r = message.send(to=details.email, smtp={"host": "smtp.gmail.com", "port": 465, "ssl": True, "user": details.user, "password": details.password, "timeout": 5})
 assert r.status_code == 250
