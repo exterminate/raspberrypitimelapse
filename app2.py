78b56b3085569152f0b0dc420f984d4387215fab
Ian <ian.coates@gmail.com>
2015-05-18 20:17:29 +0100

    update

--- app2.py
+++ app2.py
@@ -5,9 +5,9 @@ import emails
 from usernamedetails import Und
 details = Und()
 FRAMES = 24
-FPS_IN = 10
+FPS_IN = 12
 FPS_OUT = 12
-TIMEBETWEEN = 6
+TIMEBETWEEN = 12
 FILMLENGTH = float(FRAMES / FPS_IN)
 
 
@@ -18,15 +18,13 @@ while frameCount < FRAMES:
     frameCount += 1
     time.sleep(TIMEBETWEEN - 6) #Takes roughly 6 seconds to take a picture
 
-os.system("avconv -r %s -i image%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=2592:1458,scale=1280:720 timelapse.mp4"%(FPS_IN,'%7d',FPS_OUT))
-
 message = emails.html(html="<p>Hi!<br>Here is your image...",
                        subject="Image",
                        mail_from=('Ian', details.email))
 thefilename = "timelapse.mp4"
 #message.attach(data=open(thefilename), filename="timelapse.mp4")
 
-os.system("avconv -r %s -i img%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=1296:729,scale=1280:720 timelapse.mp4"%(FPS_IN,'%3d',FPS_OUT))
+os.system("avconv -r %s -i image%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=1296:729,scale=1280:720 timelapse.mp4"%(FPS_IN,'%7d',FPS_OUT))
 
 message.attach(data=open(thefilename), filename="timelapse.mp4")
 r = message.send(to=details.email, smtp={"host": "smtp.gmail.com", "port": 465, "ssl": True, "user": details.user, "password": details.password, "timeout": 5})
