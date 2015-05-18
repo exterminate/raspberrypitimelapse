f321d2877c25b8ead2bcf9a2dacdd05e38331819
Ian <ian.coates@gmail.com>
2015-05-18 20:52:39 +0100

    update

--- app2.py
+++ app2.py
@@ -27,7 +27,7 @@ message = emails.html(html="<p>Hi!<br>Here is your image...",
 thefilename = "timelapse.mp4"
 #message.attach(data=open(thefilename), filename="timelapse.mp4")
 
-os.system("avconv -r %s -i image%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=1296:729,scale=1280:720 timelapse.mp4"%(FPS_IN,'%7d',FPS_OUT))
+os.system("avconv -r %s -i image%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=1296:729,scale=1280:720 timelapse%s.mp4"%(FPS_IN,'%7d',FPS_OUT,date_now_f))
 
 message.attach(data=open(thefilename), filename="timelapse{}.mp4".format(date_now_f))
 r = message.send(to=details.email, smtp={"host": "smtp.gmail.com", "port": 465, "ssl": True, "user": details.user, "password": details.password, "timeout": 5})
