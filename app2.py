ae54820d15a4149835f6d378e2b00c88fd5a8318
Ian <ian.coates@gmail.com>
2015-05-26 20:03:06 +0000

    fixes

--- app2.py
+++ app2.py
@@ -4,11 +4,14 @@ from datetime import datetime
 import time
 import emails
 from usernamedetails import Und
+from random import randint
+
 details = Und()
+
 FRAMES = 6 #36
 FPS_IN = 6
 FPS_OUT = 6
-TIMEBETWEEN = 128
+TIMEBETWEEN = 12
 FILMLENGTH = float(FRAMES / FPS_IN)
 
 date_now = datetime.now()
@@ -26,13 +29,17 @@ while frameCount < FRAMES:
 message = emails.html(html="<p>Hi!<br>Here is your video...",
                        subject="Image",
                        mail_from=('Ian', details.email))
-thefilename = "timelapse.mp4"
+#thefilename = "timelapse.mp4"
 #message.attach(data=open(thefilename), filename="timelapse.mp4")
 
 print "{}:{}".format(datetime.now().hour,datetime.now().minute)
 
-os.system("avconv -r %s -i image%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=1296:729,scale=1280:720 timelapse%s.mp4"%(FPS_IN,'%7d',FPS_OUT,date_now_f))
+ran_num = randint(100,999)
 
-message.attach(data=open("timelapse{}.mp4".format(date_now_f)), filename="timelapse{}.mp4".format(date_now_f))
+os.system("avconv -r %s -i image%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=1296:729,scale=1280:720 timelapse%s%s.mp4"%(FPS_IN,'%7d',FPS_OUT,date_now_f,ran_num))
+
+message.attach(data=open("timelapse{}{}.mp4".format(date_now_f,ran_num)), filename="timelapse{}{}.mp4".format(date_now_f,ran_num))
 r = message.send(to=details.email, smtp={"host": "smtp.gmail.com", "port": 465, "ssl": True, "user": details.user, "password": details.password, "timeout": 5})
 assert r.status_code == 250
+
+#os.remove("*.jpg")
